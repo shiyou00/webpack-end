6aae9559d117666504fb429421835c67db934449
Frankshi <frankshi@easyops.cn>
2019-12-16 17:02:54 +0800

    feat(static): image font css scss less的配置

--- lesson_2/src/index.js
+++ lesson_2/src/index.js
@@ -1,7 +1,16 @@
+import styles from "./index.scss";
+import Icon from './icon.png';
+
 function component() {
   var element = document.createElement('div');
 
+  element.className = styles.test;
   element.innerHTML = "hello webpack";
+
+  var myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
 
   return element;
 }
