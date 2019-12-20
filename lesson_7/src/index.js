5815f69188d62dcd80da97ca6e5330fd6f3db02f
Frankshi <frankshi@easyops.cn>
2019-12-20 18:17:38 +0800

    feat(react): 解析react

--- lesson_7/src/index.js
+++ lesson_7/src/index.js
@@ -1,8 +1,10 @@
-const arr = [
-  new Promise(()=>{}),
-  new Promise(()=>{})
-];
+import React,{Component} from 'react';
+import ReactDom from 'react-dom';
 
-arr.map((item)=>{
-  console.log(item);
-})
+class App extends Component{
+  render(){
+    return <div>Hello React</div>
+  }
+}
+
+ReactDom.render(<App />, document.getElementById('root'));
