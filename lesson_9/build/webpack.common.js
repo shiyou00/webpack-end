7902b59c88fa2135c489b91e29a9d8126b9e95eb
Frankshi <frankshi@easyops.cn>
2019-12-25 10:54:14 +0800

    feat(merge): 按环境分配置文件

---
+++ lesson_9/build/webpack.common.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin/dist/clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module:{
+    rules:[
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test:/\.css$/,
+        use:[
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test:/\.(png|svg|jpg|gif)$/,
+        use:{
+          loader: 'url-loader',
+          options: {
+            name: '[name]_[hash].[ext]',
+            outputPath:"images",
+            limit: 204800
+          }
+        }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new CleanWebpackPlugin()
+  ]
+};
---
+++ lesson_9/src/math.js
@@ -0,0 +1,7 @@
+export const add = (a,b)=>{
+  console.log('add',a+b);
+}
+
+export const minus = (a,b)=>{
+  console.log('minus',a-b);
+}
